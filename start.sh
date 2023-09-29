1813236c45d576ce6d9b3c8552223d1bf73f57af
Mwangi Micha <michameiu@gmail.com>
2023-09-29 13:52:05 +0000

    Add

--- start.sh
+++ start.sh
@@ -3,11 +3,18 @@
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$(cat $ACCESS_TOKEN_FILE)
 
+
+echo "Bearer ${HELLODA} ${ACCESS_TOKEN}"
+
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --ephemeral
+pwd
+
+echo "https://github.com/${ORGANIZATION} - ${REG_TOKEN}"
+
+./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --ephemeral --labels $LABELS
 
 cleanup() {
   echo "Removing runner..."
